df6c2a2a9bebfe3690a5c4ef1d2ab1e70edc6bb8
Ben <codename-B@users.noreply.github.com>
2025-10-24 16:56:04 +0100

    formatting

--- src/vs_plugin.js
+++ src/vs_plugin.js
@@ -9,19 +9,16 @@ const patchBoneAnimator = require("./patches/boneAnimatorPatch.js");
 
 const fs = requireNativeModule('fs');
 const path = require('path');
-const Ajv = require("ajv")
+const Ajv = require("ajv");
 const process = requireNativeModule('process', {
-    message: 'This permission is required to access the VINATGE_STORY environment variable to setup texture paths.'
+    message: 'This permission is required to access the VINTAGE_STORY environment variable to setup texture paths.'
 });
 
-
-
-
-let exportAction
-let importAction
-let reExportAction
-let debugAction
-let onGroupAdd
+let exportAction;
+let importAction;
+let reExportAction;
+let debugAction;
+let onGroupAdd;
 
 Plugin.register('vs_plugin', {
     title: 'Vintage Story Format Support',
@@ -42,7 +39,6 @@ Plugin.register('vs_plugin', {
             icon: "fa-folder-plus",
             value: Settings.get("asset_path") || process.env.VINTAGE_STORY || null,
             click() {
-
                 new Dialog("gamePathSelect", {
                     title: "Select Game Path",
                     form: {
@@ -51,28 +47,24 @@ Plugin.register('vs_plugin', {
                             type: "folder",
                             value: Settings.get("game_path") || process.env.VINTAGE_STORY || "",
                         }
-
                     },
                     onConfirm(formResult) {
                         game_path_setting.set(formResult.path);
-                        Settings.save()
+                        Settings.save();
                     }
                 }).show();
-
             }
         })
 
         onGroupAdd = function(_group) {
-
-            let group = Group.first_selected
-            let parent = group.parent
-            if(parent.hologram) {
-
-                group.stepParentName = parent.name.substring(0,parent.name.length - 6)
+            let group = Group.first_selected;
+            let parent = group.parent;
+            if (parent.hologram) {
+                group.stepParentName = parent.name.substring(0, parent.name.length - 6);
             }
         }
 
-        Blockbench.on('add_group',onGroupAdd);
+        Blockbench.on('add_group', onGroupAdd);
 
         let codecVS = new Codec("codecVS", {
             name: "Vintage Story Codec",
@@ -87,41 +79,37 @@ Plugin.register('vs_plugin', {
                 }
             },
             compile(options) {
-                resetStepparentTransforms()
-                return ex(options)
+                resetStepparentTransforms();
+                return ex(options);
             },
             parse(data, file_path, add) {
-                im(data, file_path, false)
-                loadBackDropShape()
-                resolveStepparentTransforms()
+                im(data, file_path, false);
+                loadBackDropShape();
+                resolveStepparentTransforms();
             },
         })
 
         function loadBackDropShape() {
-            let backDrop = {}
-            editor_backDropShapeProp.copy(Project, backDrop)
+            let backDrop = {};
+            editor_backDropShapeProp.copy(Project, backDrop);
             if (backDrop.backDropShape) {
                 Blockbench.read(util.get_shape_location(null, backDrop.backDropShape), {
                     readtype: "text", errorbox: false
                 }, (files) => {
-                    im(files[0].content, files[0].path, true)
-                })
-
+                    im(files[0].content, files[0].path, true);
+                });
             }
         }
 
-
-
         function resolveStepparentTransforms() {
             for (var g of Group.all) {
-                let p = {}
-                props.stepParentProp.copy(g, p)
+                let p = {};
+                props.stepParentProp.copy(g, p);
                 if (p.stepParentName) {
-                    let spg = Group.all.find(group => group.name === (p.stepParentName + "_group"))
+                    let spg = Group.all.find(group => group.name === (p.stepParentName + "_group"));
                     if (spg) {
-                        let sp = spg.children[0]
-
-                        util.setParent(g, sp)
+                        let sp = spg.children[0];
+                        util.setParent(g, sp);
                         g.addTo(spg);
                     }
                 }
@@ -130,29 +118,26 @@ Plugin.register('vs_plugin', {
 
         function resetStepparentTransforms() {
             for (var g of Group.all) {
-                let p = {}
-                props.stepParentProp.copy(g, p)
+                let p = {};
+                props.stepParentProp.copy(g, p);
                 if (!g.hologram) {
-                    let spg = Group.all.find(group => group.name === (p.stepParentName + "_group"))
+                    let spg = Group.all.find(group => group.name === (p.stepParentName + "_group"));
                     if (spg) {
-                        let sp = spg.children[0]
-
-                        util.removeParent(g, sp)
+                        let sp = spg.children[0];
+                        util.removeParent(g, sp);
                         g.addTo(null);
                     }
                 }
             }
         }
 
-        let formatVS = format_definition(codecVS)
-        codecVS.format = formatVS
-
+        let formatVS = format_definition(codecVS);
+        codecVS.format = formatVS;
 
         exportAction = new Action('exportVS', {
             name: 'Export into VS Format',
             icon: 'fa-cookie-bite',
             click: function () {
-
                 Blockbench.export({
                     name: Project.name,
                     type: 'json',
@@ -160,11 +145,8 @@ Plugin.register('vs_plugin', {
                     content: codecVS.compile(),
                 });
             }
-
-        })
+        });
         MenuBar.addAction(exportAction, 'file.export');
-
-
 
         importAction = new Action('importVS', {
             name: 'Import from VS Format',
@@ -174,11 +156,10 @@ Plugin.register('vs_plugin', {
                     type: 'json',
                     extensions: ['json'],
                 }, function (files) {
-                    codecVS.parse(files[0].content)
+                    codecVS.parse(files[0].content);
                 });
             }
-
-        })
+        });
         MenuBar.addAction(importAction, 'file.import');
 
         reExportAction = new Action("reExport", {
@@ -200,30 +181,29 @@ Plugin.register('vs_plugin', {
                         let test_files = fs.readdirSync(test_folder);
                         for (var test_file of test_files) {
                             if (!test_file.includes("reexport")) {
-                                let project = new ModelProject({ format: formatVS })
-                                project.select()
+                                let project = new ModelProject({ format: formatVS });
+                                project.select();
                                 try {
                                     Blockbench.read(test_folder + path.sep + test_file, { readtype: "text", errorbox: false }, (files) => {
-                                        console.log("Importing " + test_file)
+                                        console.log("Importing " + test_file);
                                         codecVS.parse(files[0].content, test_folder + path.sep + test_file, false);
-                                        console.log("Exporting " + test_file)
-                                        let reexport_content = codecVS.compile()
-                                        let reexport_path = test_folder + path.sep + "reexport_" + path.basename(test_file)
+                                        console.log("Exporting " + test_file);
+                                        let reexport_content = codecVS.compile();
+                                        let reexport_path = test_folder + path.sep + "reexport_" + path.basename(test_file);
                                         Blockbench.writeFile(reexport_path, {
                                             content: reexport_content,
                                             savetype: "text"
-                                        })
+                                        });
                                     });
                                     //fs.writeFileSync(test_folder + path.sep  + "diff_" + path.basename(test_file), jsonDiff.diffString(JSON.parse(content),JSON.parse(reexport_content), { precision: 3}))
                                 } catch (e) {
                                     console.error(e);
                                 }
-                                project.close(true)
+                                project.close(true);
                             }
                         }
                     }
                 }).show();
-
             }
         });
         MenuBar.addAction(reExportAction, "file");
@@ -232,7 +212,7 @@ Plugin.register('vs_plugin', {
             name: 'Print Debug Info',
             icon: 'icon',
             click: function () {
-                console.log(Outliner.selected)
+                console.log(Outliner.selected);
             }
         });
         MenuBar.addAction(debugAction, "edit");
@@ -242,15 +222,15 @@ Plugin.register('vs_plugin', {
         exportAction.delete();
         importAction.delete();
         reExportAction.delete();
-        debugAction.delete()
-        Blockbench.removeListener('add_group', onGroupAdd)
+        debugAction.delete();
+        Blockbench.removeListener('add_group', onGroupAdd);
     }
 });
 
-function validate_json(content) {   
-    const ajv = new Ajv()
-    const validate = ajv.compile(vs_schema)
-    const valid = validate(content)
-    if (!valid) console.log(validate.errors)
+function validate_json(content) {
+    const ajv = new Ajv();
+    const validate = ajv.compile(vs_schema);
+    const valid = validate(content);
+    if (!valid) console.log(validate.errors);
     return valid;
 }
