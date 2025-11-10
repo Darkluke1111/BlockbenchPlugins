f3802833aaa7239aeeef7155f6b902d3fcde745a
Darkluke1111 <lukas.gromer@gmx.de>
2025-11-10 16:56:00 +0100

    add action to import a new backdrop

--- src/actions.ts
+++ src/actions.ts
@@ -1,6 +1,8 @@
 import { createAction } from "./util/moddingTools";
 import * as PACKAGE from "../package.json";
 import { is_vs_project } from "./util";
+import { im } from "./import";
+import { is_backdrop_project } from "./util/misc";
 
 
 const export_action = createAction(`${PACKAGE.name}:export_vs`, {
@@ -36,8 +38,29 @@ const import_action = createAction(`${PACKAGE.name}:import_vs`, {
             extensions: ['json'],
         }, function (files) {
             // codec and parse should be valid if action condition is met
-            Format.codec!.parse!(files[0].content, files[0].path);
+            im(autoParseJSON(files[0].content as string), files[0].path, false);
         });
     }
 });
 MenuBar.addAction(import_action, 'file.import');
+
+const import_backdrop_action = createAction(`${PACKAGE.name}:import_backdrop_action`, {
+    name: 'Import Backdrop from VS Format',
+    icon: 'fa-cookie-bite',
+    condition() {
+        return is_vs_project();
+    },
+    click: function () {
+        Blockbench.import({
+            type: 'json',
+            extensions: ['json'],
+        }, function (files) {
+            if (is_backdrop_project()) {
+                Blockbench.showQuickMessage("There is already a backdrop in this project.");
+            } else {
+                im(autoParseJSON(files[0].content as string), files[0].path, true);
+            }
+        });
+    }
+});
+MenuBar.addAction(import_backdrop_action, 'file.import');
