725dfd912ba608a5c46e06f106161b40b07e7de6
Darkluke1111 <lukas.gromer@gmx.de>
2025-11-10 15:03:50 +0100

    remove offset on export

--- src/export_model.ts
+++ src/export_model.ts
@@ -10,19 +10,6 @@ export function export_model(): Array<VS_Element> {
     const topLevelNodes = Outliner.root;
 
     let offset: [number,number,number] = [0, 0, 0];
-    const rootGroup = topLevelNodes.filter(node => node instanceof Group).map(node => node as Group)[0];
-
-    if (Project && Project.format && Project.format.id === 'formatVS') {
-        // VS format: ensure root exports at (8,0,8)
-        offset = (rootGroup && rootGroup.origin[0] === 8 && rootGroup.origin[1] === 0 && rootGroup.origin[2] === 8)
-            ? [0, 0, 0]
-            : [8, 0, 8];
-    } else {
-        // Non-VS format (legacy fallback)
-        offset = (rootGroup && rootGroup.origin[0] === 8 && rootGroup.origin[1] === 0 && rootGroup.origin[2] === 8)
-            ? [0, 0, 0]
-            : [8, 0, 8];
-    }
 
     traverse(null, topLevelNodes, elements, offset);
     return elements;
